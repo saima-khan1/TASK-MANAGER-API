1957985c3f82f105c86a6f519aed962630c9ec77
Saima Sanjeeda Khan <119809514+saima-khan1@users.noreply.github.com>
2025-08-30 00:22:53 +0200

    connected to mongodb

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -1,6 +1,8 @@
 import express from "express";
 import cors from "cors";
 import * as dotenv from "dotenv";
+import mongoose from "mongoose";
+import { error } from "console";
 
 dotenv.config();
 const app = express();
@@ -9,10 +11,21 @@ const PORT = 3002;
 app.use(express.json());
 app.use(cors());
 
+const connectDB = async () => {
+  try {
+    mongoose.set("strictQuery", true);
+    await mongoose.connect(process.env.MONGODB_URL as string);
+    console.log("connected to mongoDB");
+  } catch (err) {
+    console.error("❌ Failed to connect MongoDB:", err);
+  }
+};
+
 app.get("/", (req, res) => {
   res.send("hello");
 });
 
-app.listen(PORT, () => {
+app.listen(PORT, async () => {
   console.log(`Backend running at http://localhost:${PORT}`);
+  await connectDB();
 });
