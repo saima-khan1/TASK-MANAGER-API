d59453d4e055b94ad1bb21a92016accdf673b05f
Saima Sanjeeda Khan <119809514+saima-khan1@users.noreply.github.com>
2025-08-31 23:40:21 +0200

    created schema

---
+++ backend/src/models/Task.ts
@@ -0,0 +1,31 @@
+import mongoose, { Schema, Document } from "mongoose";
+
+export interface Task extends Document {
+  title: string;
+  description: string;
+  status: "pending" | "in-progress" | "completed";
+  priority: "low" | "medium" | "high";
+  dueDate?: Date;
+  createdAt: Date;
+  updatedAt: Date;
+}
+const TaskSchema: Schema = new Schema<Task>(
+  {
+    title: { type: String, required: true },
+    description: { type: String },
+    status: {
+      type: String,
+      enum: ["pending", "in-progress", "completed"],
+      default: "pending",
+    },
+    priority: {
+      type: String,
+      enum: ["low", "medium", "high"],
+      default: "medium",
+    },
+    dueDate: { type: Date },
+  },
+  { timestamps: true }
+);
+console.log(TaskSchema, "TaskSchema");
+export default mongoose.model<Task>("Task", TaskSchema);
