7cae3f68bfe9c1059438cf871bbffdab1b057ae3
Saima Sanjeeda Khan <119809514+saima-khan1@users.noreply.github.com>
2025-09-01 16:22:52 +0200

    added delete and put operations

--- backend/src/controllers/Task.ts
+++ backend/src/controllers/Task.ts
@@ -13,6 +13,16 @@ export const getTask = async (req: Request, res: Response) => {
     res.status(200).json({ sucess: true, data: task });
   } catch (err) {
     console.error("failed to get data ");
+    res.status(500).json({ error: "Failed to fetch data" });
+  }
+};
+export const getTaskId = async (req: Request, res: Response) => {
+  try {
+    const task = await Task.findById(req.params.id);
+    res.status(200).json({ sucess: true, data: task });
+  } catch (err) {
+    console.error("failed to get data ");
+    res.status(500).json({ error: "Failed to fetch data" });
   }
 };
 
@@ -21,7 +31,30 @@ export const createPost = async (req: Request, res: Response) => {
     const task = await Task.create(req.body);
     task.save();
     res.status(201).json({ sucess: true, data: task });
+    console.log(task, "task");
   } catch (err) {
     console.error("failed to post ");
+    res.status(500).json({ error: "Failed to fetch data" });
+  }
+};
+
+export const updatingTask = async (req: Request, res: Response) => {
+  try {
+    const task = await Task.findByIdAndUpdate(req.params.id, req.body, {
+      new: true,
+    });
+    res.status(201).json({ sucess: true, data: task });
+    console.log(task, "task");
+  } catch (error) {
+    console.error("failed to update");
+  }
+};
+
+export const deleteTask = async (req: Request, res: Response) => {
+  try {
+    const task = await Task.findByIdAndDelete(req.params.id);
+    res.status(200).json({ sucess: true, data: task });
+  } catch (err) {
+    console.error("failed to update");
   }
 };
--- backend/src/routes/Task.ts
+++ backend/src/routes/Task.ts
@@ -1,8 +1,17 @@
 import express from "express";
-import { createPost, getTask } from "../controllers/Task";
+import {
+  createPost,
+  deleteTask,
+  getTask,
+  getTaskId,
+  updatingTask,
+} from "../controllers/Task";
 
 const router = express.Router();
 router.get("/", getTask);
+router.get("/:id", getTaskId);
 router.post("/", createPost);
+router.put("/:id", updatingTask);
+router.delete("/:id", deleteTask);
 
 export default router;
